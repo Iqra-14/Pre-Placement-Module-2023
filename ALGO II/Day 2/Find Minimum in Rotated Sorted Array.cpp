d26bb886d93dd4343cdaa80844efe46eb3402211
Iqra-14 <iqraaamin14@gmail.com>
2022-07-23 13:42:22 +0530

    Updated W4 Day2 Day3.cpp

---
+++ ALGO II/Day 2/Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+    int start = 0, end = nums.size()-1, mid;
+    while (nums[start] > nums[end]) {
+        mid = (start + end) / 2;
+        if (nums[mid] > nums[start])
+            start = mid+1;
+        else {
+            ++start;
+            end = mid;
+        }
+    }
+    return nums[start];
+    }
+};
